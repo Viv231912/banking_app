e5c4c0c09117ba9ff8b46cb2f2b1cf3f26124987
Vivian <schen231912@gmail.com>
2024-10-13 21:33:14 +1100

    add right-sidebar and total-balancebox to home-page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,38 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { HeadManagerContext } from 'next/dist/server/future/route-modules/app-page/vendored/contexts/entrypoints'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn ={ firstName:"Viv", lastName:"C",email:"sample@email.com"}
+  
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+        <RightSidebar
+          user={loggedIn}
+          transactions={[]}
+          banks={[{currentBalance: 123.98}, {currentBalance: 123.98}]}
+/>
+
+    </section>
   )
 }
 
